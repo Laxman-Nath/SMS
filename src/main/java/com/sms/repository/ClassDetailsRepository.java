2fe35929af42979944af69a5773d7c5c312e488f
Laxman-Nath <nathlaxman242@gmail.com>
2025-09-17 19:52:39 +0545

    feat : create database access classes

---
+++ src/main/java/com/sms/repository/ClassDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.sms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sms.models.ClassDetails;
+
+public interface ClassDetailsRepository extends JpaRepository<ClassDetails, Integer> {
+
+}
---
+++ src/main/java/com/sms/repository/ClassRepository.java
@@ -0,0 +1,10 @@
+package com.sms.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ClassRepository extends JpaRepository<com.sms.models.Class,Integer>{
+    
+}
